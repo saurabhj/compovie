f2347c5b23c71e29298c84b40c9cdf7e51428690
Saurabh Jain <s.jain@openenergymarket.com>
2015-10-28 11:33:36 +0530

    First commit.

---
+++ src/js/main.js
@@ -0,0 +1,130 @@
+﻿var TOTAL_MOVIES = 0;
+var HIGHEST_RATING = 0;
+
+$(document).ready(function () {
+    // Focus the search text box on page load
+    $("#txtSearch").focus();
+
+    // Instantiate the autocomplete for the search box
+    $("#txtSearch").autocomplete({
+        minChars: 2,
+        preventBadQueries: false,
+        lookup: function (query, done) {
+            $("#divSearch").addClass("loading");
+
+            $.ajax({
+                url: "http://www.omdbapi.com/",
+                dataType: "jsonp",
+                data: {
+                    s: query.trim(),
+                    type: "movie"
+                },
+                success: function (data) {
+                    $("#divSearch").removeClass("loading");
+                    var completeArray = _.map(data.Search, function (item) {
+                        return {
+                            value: item.Title + " (" + item.Year + ")",
+                            data: item.imdbID
+                        }
+                    });
+
+                    var suggestions = [];
+                    $.each(completeArray, function (index, item) {
+                        if (_.findWhere(suggestions, { data: item.data }) == undefined) {
+                            suggestions.push(item);
+                        }
+                    });
+
+                    var result = {};
+                    result.suggestions = suggestions;
+                    done(result);
+                }
+            }).fail(function () {
+                $("#divSearch").removeClass("loading");
+            });
+        },
+        onSelect: function (suggestion) {
+            addMovie(suggestion.data);
+
+            $("#txtSearch").val("");
+            $("#txtSearch").focus();
+        }
+    });
+});
+
+// Query the API and pull complete information on the film
+function addMovie(imdbId) {
+    var plotLength = "short";
+    if ($("#chkLongerPlot").prop("checked") == true) {
+        plotLength = "full";
+    }
+
+    $.ajax({
+        url: "http://www.omdbapi.com/",
+        dataType: "jsonp",
+        data: {
+            i: imdbId,
+            plot: plotLength,
+            r: "json",
+            tomatoes: true
+        },
+        success: function (data) {
+            addMovieToTable(data);
+        }
+    }).fail(function () {
+    });
+}
+
+// Render the UI
+function addMovieToTable(data) {
+    if (TOTAL_MOVIES == 0) {
+        $("#divResults").html(Templates.movie.header);
+        $("#divResults").append(Templates.movie.footer);
+        $('.popup').popup({ inline: true });
+    }
+
+    var template = Handlebars.compile(Templates.movie.body);
+    var html = template(data);
+
+    $("#tabMovies > tbody:last-child").append(html);
+    TOTAL_MOVIES++;
+
+    if (data.imdbRating != "N/A") {
+        var rating = parseFloat(data.imdbRating);
+        if (HIGHEST_RATING < rating) {
+            HIGHEST_RATING = rating;
+        }
+    }
+
+    if (TOTAL_MOVIES > 1) {
+        $("#tabMovies > tbody > tr").removeClass("positive");
+        $.each($("#tabMovies > tbody > tr"), function (index, value) {
+            if ($(value).data("rating") != "N/A" && parseFloat($(value).data("rating")) == HIGHEST_RATING) {
+                $(value).addClass("positive");
+            }
+        });
+    }
+
+    var templateRT = Handlebars.compile(Templates.movie.rottenTomatoesBox);
+
+    // Adding more information for IMDB
+    $("#btnRTMoreInfo-" + data.imdbID).popup({
+        title: "More information",
+        html: templateRT(data)
+    });
+}
+
+/*
+ * Util Methods
+*/
+
+Number.prototype.formatNumber = function (c, d, t) {
+    var n = this,
+        c = isNaN(c = Math.abs(c)) ? 2 : c,
+        d = d == undefined ? "." : d,
+        t = t == undefined ? "," : t,
+        s = n < 0 ? "-" : "",
+        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
---
+++ src/js/templates/template.movie.js
@@ -0,0 +1,95 @@
+﻿Templates.movie = {};
+
+Templates.movie.header = [
+    "<table id=\"tabMovies\" class=\"ui striped stackable table\">",
+        "<thead>",
+            "<tr>",
+                "<th colspan=\"2\">Film</th>",
+                "<th style=\"width: 110px\" class=\"center aligned icon-header\">",
+                    "<img class=\"popup\" data-title=\"IMDB Rating\" alt=\"IMDB\" src=\"images/imdb_logo.png\"></img>",
+                "</th>",
+                "<th style=\"width: 110px\" class=\"center aligned icon-header\">",
+                    "<img class=\"popup\" data-title=\"Rotten Tomatoes Rating\" alt=\"Rotten Tomatoes\" src=\"images/rt_logo.png\"></img>",
+                "</th>",
+                "<th style=\"width: 110px\" class=\"center aligned icon-header\">",
+                    "<img class=\"popup\" data-title=\"Metacritic Rating\" alt=\"Metacritic\" src=\"images/metacritic_logo.png\"></img>",
+                "</th>",
+            "</tr>",
+        "</thead>",
+        "<tbody>"
+].join("\n");
+
+Templates.movie.body = [
+    "<tr id=\"tr-{{imdbID}}\" data-rating=\"{{imdbRating}}\">",
+        "<td style=\"width: 120px\" class=\"center top aligned\">",
+            "<img src=\"http://img.omdbapi.com/?i={{imdbID}}&apikey=30bfd0c9&h=150\"></img>",
+        "</td>",
+        "<td class=\"top aligned\">",
+            "<strong>{{Title}} ({{Year}})</strong><br />",
+            "<strong>Rated:</strong> {{Rated}}, <strong>Runtime:</strong> {{Runtime}}<br />",
+            "<em>{{Plot}}</em><br />",
+            "<br />",
+            "<strong>Directed by:</strong> {{Director}}<br />",
+            "<strong>Starring:</strong> {{Actors}}",
+        "</td>",
+        "<td class=\"center aligned\">",
+            "<div class=\"rating-box\">",
+                "<strong>{{imdbRating}} / 10</strong><br />",
+                "<small>({{imdbVotes}}&nbsp;votes)</small><br />",
+            "</div>",
+            "<div class=\"more-info-box\">",
+                "<a class=\"ui small orange circular icon button\" href=\"http://www.imdb.com/title/{{imdbID}}/\" target=\"_blank\">",
+                    "<i class=\"external square icon\"></i>",
+                "</a>",
+            "</div>",
+        "</td>",
+        "<td class=\"center aligned\">",
+            "<div class=\"rating-box\">",
+                "<strong>{{tomatoMeter}}%</strong><br />",
+            "</div>",
+            "<div class=\"more-info-box\">",
+                "<button class=\"ui small orange circular icon button\" id=\"btnRTMoreInfo-{{imdbID}}\">",
+                    "<i class=\"info icon link\"></i>",
+                "</button>",
+                "<a class=\"ui small orange circular icon button\" href=\"http://www.rottentomatoes.com/m/{{generateRTString Title}}\" target=\"_blank\">",
+                    "<i class=\"external square icon\"></i>",
+                "</a>",
+            "</div>",
+        "</td>",
+        "<td class=\"center aligned\">",
+            "<div class=\"rating-box\">",
+                "<strong>{{Metascore}}</strong>",
+            "</div>",
+            "<div class=\"more-info-box\">",
+                "<a class=\"ui small orange circular icon button\" href=\"http://www.metacritic.com/movie/{{generateMetacriticString Title}}\" target=\"_blank\">",
+                    "<i class=\"external square icon\"></i>",
+                "</a>",
+            "</div>",
+        "</td>",
+    "</tr>"
+].join("\n");
+
+Templates.movie.footer = [
+        "</tbody>",
+    "</table>"
+].join("\n");
+
+Templates.movie.rottenTomatoesBox = [
+    "<strong>Fresh:</strong> {{tomatoFresh}} / {{tomatoReviews}}<br />",
+    "<strong>User Rating:</strong> {{tomatoUserMeter}}%",
+    "<small>({{addCommas tomatoUserReviews}} reviews)</small><br />",
+    "<br />",
+    "<strong><small><em>&ldquo;{{tomatoConsensus}}&rdquo;</em></small></strong><br />",
+].join("\n");
+
+Handlebars.registerHelper("generateRTString", function (title) {
+    return title.replace(/[^A-Z0-9]+/ig, "_").toLowerCase();
+});
+
+Handlebars.registerHelper("generateMetacriticString", function (title) {
+    return title.replace(/[^A-Z0-9]+/ig, "-").toLowerCase();
+});
+
+Handlebars.registerHelper("addCommas", function (number) {
+    return parseInt(number).formatNumber(0);
+});
